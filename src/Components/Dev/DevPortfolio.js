dd68e23c11aa8075d04762249554dd6336cdca4a
koukoumpitsa <d.chantzopoulou@gmail.com>
2021-03-22 09:40:53 +0100

    DevPortfolio styled-components seperation

--- src/Components/Dev/DevPortfolio.js
+++ src/Components/Dev/DevPortfolio.js
@@ -1,67 +1,7 @@
 import React, {useState} from "react";
 import { useSpring, animated } from "react-spring";
 import Confetti from "react-dom-confetti";
-<<<<<<< HEAD:src/Components/Dev/DevPortfolio.js
 import {DevPortfolioWrapper} from "./DevPortfolioStyling";
-=======
-import styled from "styled-components";
-
-const DevPortfolioWrapper = styled.div`
-    height: 70vh;
-    margin: 20px;
-    display: grid;
-    place-items: center;
-
-    .body{
-        height: 60%;
-        width: 70%;
-        display: grid;
-        grid-template-rows: 1fr 1fr 1fr;
-        color: black;
-
-        .body-text-title{
-            display: grid;
-            place-items: center start;
-
-            .title{
-                font-size: 70px;
-                font-weight: 800;
-            }
-        }
-
-        .body-text{
-            font-size: 40px;
-
-            .confetti{
-                font-weight: 700;
-            }
-        }
-
-        .body-confetti{
-            display: grid;
-            place-items: center;
-
-            button{
-                background-color: black;
-                color: white;
-                padding: 20px 30px 20px 30px;
-                border-radius: 4px;
-                border: none;
-                letter-spacing: 1px;
-
-                // :hover{
-                //     background-color: white;
-                //     border: 2px solid black;
-                //     letter-spacing: 1px;
-                //     font-weight: 700;
-                //     color: black;
-                // }
-            }
-        }
-
-    }
-`;
->>>>>>> 3d3f610e218361fd2d0591bd384b0234c52cb748:src/DevPortfolio.js
 
 export default function DevPortfolio(){
     const props = useSpring({
