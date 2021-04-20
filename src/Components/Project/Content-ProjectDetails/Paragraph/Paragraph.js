911adf5c5599b15c1fc3bb2f3e8af3b7fde5e195
koukoumpitsa <d.chantzopoulou@gmail.com>
2021-04-20 10:25:07 +0200

    corrections

--- src/Components/Project/Content-ProjectDetails/Paragraph/Paragraph.js
+++ src/Components/Project/Content-ProjectDetails/Paragraph/Paragraph.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import PageDescription from "./../../../Page/Description/PageDescription";
 
 const ParagraphWrapper = styled.div`
 
