fa50ee07408a118d5dd8a0a026ccad7f90dd28f1
koukoumpitsa <d.chantzopoulou@gmail.com>
2021-04-27 10:26:33 +0200

    delete bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './styles/App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./styles/App.css";
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from "react-router-dom";
 import NavBar from "./Components/Navbar/NavBar.js";
-import Home from './Pages/Home';
-import Skills from './Pages/Skills/Skills';
-import Experience from './Pages/Experience/Experience';
-import Projects from './Pages/Projects/Projects';
+import Home from "./Pages/Home";
+import Skills from "./Pages/Skills/Skills";
+import Experience from "./Pages/Experience/Experience";
+import Projects from "./Pages/Projects/Projects";
 
 export default function App() {
   return (
@@ -14,8 +14,7 @@ export default function App() {
       <Home />
       <Skills />
       <Experience />
-      <Projects/>
+      <Projects />
     </Router>
   );
 }
-
