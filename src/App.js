7194fd7460c81ed05a3e16ae3969e2431af064d2
koukoumpitsa <d.chantzopoulou@gmail.com>
2021-04-28 22:39:48 +0200

    delete old files

--- src/App.js
+++ src/App.js
@@ -1,29 +1,6 @@
 import "./helpers/App.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./pages/Navbar";
-import Home from "./pages/Home";
-import Skills from "./pages/Skills";
-import Experience from "./pages/Experience";
-import Projects from "./pages/Projects";
-import Footer from "./pages/Footer";
-import { Container } from "./Components/Containers";
-import Weather from "./api/Weather";
 
 export default function App(props) {
-  return (
-    <Router>
-      <Container>
-        <Navbar
-          handleThemeChange={props.handleThemeChange}
-          colorTheme={props.colorTheme}
-        />
-        <Home />
-        <Weather />
-        <Skills />
-        <Experience />
-        <Projects />
-        <Footer />
-      </Container>
-    </Router>
-  );
+  return <Router></Router>;
 }
--- src/Components/Button.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Button = styled.button`
-    border: none;
-    background-color: ${props => props.theme.color.background};
-    color: ${props => props.theme.color.text};
-    cursor: pointer;
-`;
