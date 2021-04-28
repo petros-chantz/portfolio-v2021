c3fadd01e77c052b731d2bbd40d4319433f34093
koukoumpitsa <d.chantzopoulou@gmail.com>
2021-04-28 22:39:48 +0200

    delete old files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
 import "./helpers/App.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./pages/Navbar";
-import Home from "./pages/Home";
-import Skills from "./pages/Skills";
-import Experience from "./pages/Experience";
-import Projects from "./pages/Projects";
-import Footer from "./pages/Footer";
-import { Container } from "./Components/Containers";
-import Weather from "./api/Weather";
 
 export default function App(props) {
   return (
