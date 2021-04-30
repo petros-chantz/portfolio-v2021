a393076bce6ff9f4e0b4ba0571bc81a404ae5412
koukoumpitsa <d.chantzopoulou@gmail.com>
2021-04-30 17:13:43 +0200

    added conditional render of components depending on colorTheme and extralinks truthy or falsy

--- src/components/Projects/ProjectItemLinks.js
+++ src/components/Projects/ProjectItemLinks.js
@@ -1,9 +1,31 @@
 import React from "react";
 import { Container } from "../Container";
+import IconListDark2 from "./IconLists/IconListDark2";
+import IconListDark3 from "./IconLists/IconListDark3";
+import IconListLight2 from "./IconLists/IconListLight2";
+import IconListLight3 from "./IconLists/IconListLight3";
 
 export default function ProjectItemLinks(props) {
   const { project } = props;
   return (
-    <Container columns="repeat(3, 100px)" place="center start"></Container>
+    <>
+      {props.colorTheme ? (
+        <Container columns="repeat(3, 100px)" place="center start">
+          {project.extraLinks ? (
+            <IconListLight3 project={project} />
+          ) : (
+            <IconListLight2 project={project} />
+          )}
+        </Container>
+      ) : (
+        <Container columns="repeat(3, 100px)" place="center start">
+          {project.extraLinks ? (
+            <IconListDark3 project={project} />
+          ) : (
+            <IconListDark2 project={project} />
+          )}
+        </Container>
+      )}
+    </>
   );
 }
