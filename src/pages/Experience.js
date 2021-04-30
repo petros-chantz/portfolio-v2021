740cd066f9719837ba37598bd66549136788385d
koukoumpitsa <d.chantzopoulou@gmail.com>
2021-04-30 17:48:10 +0200

    experience text change

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -37,8 +37,8 @@ export default function Experience() {
       <Container padding="0px 50px 0px 0px">
         <Quotes
           quotesText="How are all these skills useful to you? Over the past 4+ years I have
-        had the chance to work in multiple academic research projects as a research assistant, in design
-        research labs as a lab assistant and in commercial projects for freelancers and
+        had the chance to work in multiple academic research projects, in design
+        research labs and in commercial projects for
         companies such as Signify (Ex. Philips Lighting)."
         />
       </Container>
