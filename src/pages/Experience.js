14a7e9d534ddf1ae46430d7d47ae6b273ec86629
koukoumpitsa <d.chantzopoulou@gmail.com>
2021-05-01 21:21:56 +0200

    deleted component's own padding - global padding implemented

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,39 +1,28 @@
 import React from "react";
-import { Container } from "../components/Container";
+import { Padding, Container } from "../components/Container";
 import { breakpoints } from "./../helpers/Media";
 import styled from "styled-components";
 import ExperienceYearsList from "../components/Experience/ExperienceYearsList";
 import Quotes from "../components/Quotes";
 
-const PaddingExperience = styled(Container)`
+const ExperienceContainer = styled(Padding)`
   @media screen and (max-width: ${breakpoints.screen4k}) {
-    padding: 100px 500px 0px 500px;
     grid-template-rows: 800px;
     grid-template-columns: 2fr 1fr;
   }
   @media screen and (max-width: ${breakpoints.laptopLScreen}) {
-    padding: 100px 190px 0px 190px;
     grid-template-rows: 1fr;
   }
-  @media screen and (max-width: ${breakpoints.laptopMScreen}) {
-    padding: 100px 190px 0px 190px;
-  }
+
   @media screen and (max-width: ${breakpoints.laptopScreen}) {
-    padding: 100px 150px 0px 150px;
     grid-template-rows: 1fr 500px;
     grid-template-columns: 1fr;
-  }
-  @media screen and (max-width: ${breakpoints.tabletScreen}) {
-    padding: 100px 100px 0px 100px;
-  }
-  @media screen and (max-width: ${breakpoints.mobileLScreen}) {
-    padding: 100px 30px 0px 30px;
   }
 `;
 
 export default function Experience() {
   return (
-    <PaddingExperience gap="50px" id="experience">
+    <ExperienceContainer gap="50px" id="experience">
       <Container padding="0px 50px 0px 0px">
         <Quotes
           quotesText="How are all these skills useful to you? Over the past 4+ years I have
@@ -45,6 +34,6 @@ export default function Experience() {
       <Container>
         <ExperienceYearsList />
       </Container>
-    </PaddingExperience>
+    </ExperienceContainer>
   );
 }
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,38 +1,25 @@
 import React from "react";
-import { Container } from "./../components/Container";
+import { Padding } from "./../components/Container";
 import Quotes from "./../components/Quotes";
 import SkillsTopicList from "../components/Skills/SkillsTopicList";
 import { breakpoints } from "./../helpers/Media";
 import styled from "styled-components";
 import SkillsStack from "../components/Skills/SkillsStack";
 
-const SkillsPadding = styled(Container)`
+const SkillsContainer = styled(Padding)`
   @media screen and (max-width: ${breakpoints.screen4k}) {
-    padding: 100px 500px 0px 500px;
     grid-template-rows: 1fr 300px 200px;
     grid-template-columns: 1fr;
   }
-  @media screen and (max-width: ${breakpoints.laptopLScreen}) {
-    padding: 100px 190px 0px 190px;
-  }
-  @media screen and (max-width: ${breakpoints.laptopMScreen}) {
-    padding: 100px 190px 0px 190px;
-  }
+
   @media screen and (max-width: ${breakpoints.laptopScreen}) {
-    padding: 100px 150px 0px 150px;
     grid-template-rows: 1fr 1fr 200px;
-  }
-  @media screen and (max-width: ${breakpoints.tabletScreen}) {
-    padding: 100px 100px 0px 100px;
-  }
-  @media screen and (max-width: ${breakpoints.mobileLScreen}) {
-    padding: 100px 30px 0px 30px;
   }
 `;
 
 export default function Skills() {
   return (
-    <SkillsPadding gap="40px" place="center" id="skills">
+    <SkillsContainer gap="40px" place="center" id="skills">
       <Quotes
         quotesText="I wear a lot of hats and more often than not they overlap one another.
          I strongly believe that designing products, services and or systems for societal
@@ -43,6 +30,6 @@ export default function Skills() {
       />
       <SkillsTopicList />
       <SkillsStack />
-    </SkillsPadding>
+    </SkillsContainer>
   );
 }
