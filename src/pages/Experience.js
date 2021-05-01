9ac0e21cef522c8ad2230ae8ee5ba288f56318c8
koukoumpitsa <d.chantzopoulou@gmail.com>
2021-05-01 17:33:42 +0200

    delete import component

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -3,7 +3,6 @@ import { Container } from "../components/Container";
 import { breakpoints } from "./../helpers/Media";
 import styled from "styled-components";
 import ExperienceYearsList from "../components/Experience/ExperienceYearsList";
-import Quotes from "../components/Quotes";
 
 const PaddingExperience = styled(Container)`
   @media screen and (max-width: ${breakpoints.screen4k}) {
