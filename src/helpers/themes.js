fdf07499d20ecf694f36b0b03397a3cab8fca3af
koukoumpitsa <d.chantzopoulou@gmail.com>
2021-05-05 14:53:19 +0200

    removed old icon themes

--- src/helpers/themes.js
+++ src/helpers/themes.js
@@ -1,12 +1,3 @@
-// import cv from "./../images/icons/cv.svg";
-// import cvLight from "./../images/icons/cv-light.svg";
-// import email from "./../images/icons/email.svg";
-// import emailLight from "./../images/icons/email-light.svg";
-// import github from "./../images/icons/github.svg";
-// import githubLight from "./../images/icons/github-light.svg";
-// import linkedIn from "./../images/icons/linkedIn.svg";
-// import linkedInLight from "./../images/icons/linkedIn-light.svg";
-
 export const light = {
   color: {
     text: "#000000",
@@ -16,13 +7,6 @@ export const light = {
     borderSkills: "#8EB8E5",
   },
   borderRadius: "0px",
-  // icons: {
-  //   email: `${email}`,
-  //   cv: `${cv}`,
-  //   github: `${github}`,
-  //   linkedIn: `${linkedIn}`,
-  //   iconsHover: {},
-  // },
 };
 
 export const dark = {
@@ -34,10 +18,4 @@ export const dark = {
     borderSkills: "#8EB8E5",
   },
   borderRadius: "5px",
-  // icons: {
-  //   email: `${emailLight}`,
-  //   cv: `${cvLight}`,
-  //   github: `${githubLight}`,
-  //   linkedIn: `${linkedInLight}`,
-  // },
 };
