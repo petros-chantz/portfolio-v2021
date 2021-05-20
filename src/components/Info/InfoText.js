1358a970cf220cb0d4e09866c4085d4694bd2116
koukoumpitsa <d.chantzopoulou@gmail.com>
2021-05-20 11:27:01 +0200

    text-info mod

--- src/components/Info/InfoText.js
+++ src/components/Info/InfoText.js
@@ -7,9 +7,9 @@ export default function InfoText() {
       <TextLarge>
         I’m a UX Researcher and an all-around Creative Technologist. I hold a
         Master's Degree in Constructive Design Research from Technical
-        University of Eindhoven. Currently I’m passionate about frontend
-        development technologies such as React.js and with emphasis on the use
-        of data as a design material.
+        University of Eindhoven. I’m passionate about frontend development
+        technologies such as React.js with emphasis on the use of data as a
+        design material.
       </TextLarge>
       <TextLarge>I’m ready to take the next step and join your team!</TextLarge>
     </Container>
