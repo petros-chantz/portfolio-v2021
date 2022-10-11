d7831c993d618d5b108f7823c7dda00b14b21ef3
Petros <petros.chantz@gmail.com>
2022-10-11 10:23:42 +0200

    feat: updates ComingSoon page

--- src/pages/ComingSoon.js
+++ src/pages/ComingSoon.js
@@ -1,44 +1,24 @@
 import React from "react";
-import './ComingSoon.css';
+import "./ComingSoon.css";
 
 export default class ComingSoon extends React.Component {
-
-   findDateOfLaunch(){
-    //    let dateTarget = new Date('07/20/2022');
-    //    let dateToday = new Date();
-    //    let difference = dateTarget.getTime() - dateToday.getTime();
-    //    let days = Math.ceil(difference / (1000 * 3600 * 24));
-    //    return days;
-
-        let newDate = new Date()
-        let date = newDate.getDate();
-        let month = newDate.getMonth() + 1;
-
-        if(month === 4){
-            let daysLeft = (30 - date) + 31 + 30
-            return daysLeft;
-        } else if(month === 5){
-            let daysLeft = (31 - date) + 30
-            return daysLeft;
-        } else if(month === 6){
-            let daysLeft = 20 - date;
-            return daysLeft;
-        }
-
-        
-   } 
-
-   render(){
+  render() {
     return (
-        <div className="container">
+      <div className="container">
         <div className="coming-soon">
-            <h1 className="title">Oops, sorry...</h1>
-            <h3 className="subtitle">Currently working on creating a new portfolio.</h3>
-            <h3 className="subtitle">Expected Launch Date: June 20th 2022 - {this.findDateOfLaunch()} days from now.</h3>
-            <h3 className="subtitle email">petros.chantz [at] gmail [dot] com</h3>
+          <h1 className="title">Oops, sorry...</h1>
+          <h3 className="subtitle">Currently updating my portfolio.</h3>
+          <img
+            src="https://i.pinimg.com/originals/bc/53/34/bc5334475aa08579ffbf33e395cb3e34.jpg"
+            alt=""
+          />
+          <div className="footer">
+            <h3 className="subtitle email">
+              petros.chantz [at] gmail [dot] com
+            </h3>
+          </div>
         </div>
-        </div>
-      );
-   }
+      </div>
+    );
+  }
 }
-
